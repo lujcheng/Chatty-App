490acc27c44fab0f14ff8012f699b2132a44da8f
Lucas C <lucas.cheng12@gmail.com>
2019-06-04 04:39:23 +0000

    broken html into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import Navbar from "./NavBar.jsx";
+import Message from "./Message.jsx";
+import Chatbar from "./ChatBar.jsx";
+import messages from "./messages.json";
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: messages
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Navbar />
+        <Message messages={this.state.messages[0].content} />
+        <Chatbar />
+      </div>
     );
-  }
+    }
 }
 export default App;
