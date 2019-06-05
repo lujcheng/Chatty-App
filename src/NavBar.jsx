d07e5e5c0f48f50a9cf81099bd2b0f2117f4c16c
Lucas C <lucas.cheng12@gmail.com>
2019-06-05 21:11:50 +0000

    added users online in nav

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,9 +2,14 @@ import React, {Component} from 'react';
 
 class Navbar extends Component {
     render() {
+        const style = {
+            display: "flex",
+            justifyContent: "space-between"
+        }
         return (
-            <nav className="navbar">
+            <nav className="navbar" style={style} >
                 <a href="/" className="navbar-brand">Chatty</a>
+                <span className="navbar-brand" style={{fontSize: "1em"}}>{this.props.connections} users currently online </span>
             </nav>
         )
     }
